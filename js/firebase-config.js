fff5cc801613ee45e5d54cbfe3935dbe66d5236c
elcanaclinica <elcanaconta@gmail.com>
2026-05-20 20:23:44 -0300

    Delete js/firebase-config.js

--- js/firebase-config.js
+++
@@ -1,8 +0,0 @@
-const FIREBASE_CONFIG = {
-  apiKey:            "AIzaSyDT6wRcojZxatiOxqpXXv_bmqzYnR-E6GY",
-  authDomain:        "elcanaclinica.firebaseapp.com",
-  projectId:         "elcanaclinica",
-  storageBucket:     "elcanaclinica.firebasestorage.app",
-  messagingSenderId: "38758620071",
-  appId:             "1:38758620071:web:320777851e9a00066cb482"
-};
